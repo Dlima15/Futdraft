23debefded71565bc09bba80b9211d5c775c5808
Dlima15 <danilo.lima7456@gmail.com>
2025-10-17 17:26:57 -0300

    setup: integra projeto backend ao monorepo FutDraft (Spring Boot base configurada)

---
+++ futdraft-backend/backend/src/main/java/com/futdraft/FutdraftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.futdraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutdraftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutdraftBackendApplication.class, args);
+	}
+
+}
---
+++ futdraft-backend/backend/src/test/java/com/futdraft/FutdraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futdraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutdraftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
